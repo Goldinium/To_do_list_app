4a5647cd49b216dfb0ce1d31f5c1e9891ae450b5
Badr BenSlama <badrbenslama8@gmail.com>
2023-08-19 19:11:40 +0100

    Fixed linters

--- src/isCompletedAndChecked.js
+++ src/isCompletedAndChecked.js
@@ -1,19 +1,19 @@
 const isCompletedAndChecked = (checkbox, i) => {
-    if (this.taskList[i].completed) {
-      checkbox.checked = true;
-      document.querySelector(`.content${i}`).classList.add('completed');
-    }
-
-    checkbox.addEventListener('change', () => {
-      if (checkbox.checked) {
-        document.querySelector(`.content${i}`).classList.add('completed');
-        this.taskList[i].completed = true;
-        localStorage.setItem('tasks', JSON.stringify(this.taskList));
-      } else {
-        document.querySelector(`.content${i}`).classList.remove('completed');
-        this.taskList[i].completed = false;
-        localStorage.setItem('tasks', JSON.stringify(this.taskList));
-      }
-    });
+  if (this.taskList[i].completed) {
+    checkbox.checked = true;
+    document.querySelector(`.content${i}`).classList.add('completed');
   }
+
+  checkbox.addEventListener('change', () => {
+    if (checkbox.checked) {
+      document.querySelector(`.content${i}`).classList.add('completed');
+      this.taskList[i].completed = true;
+      localStorage.setItem('tasks', JSON.stringify(this.taskList));
+    } else {
+      document.querySelector(`.content${i}`).classList.remove('completed');
+      this.taskList[i].completed = false;
+      localStorage.setItem('tasks', JSON.stringify(this.taskList));
+    }
+  });
+};
 export default isCompletedAndChecked;
